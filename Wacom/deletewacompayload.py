5306cdafe3744d009eef0427a58d4c6af1b89550
aitorhergon <hdezaitor@gmail.com>
2023-03-07 11:36:50 +0100

    Update deletewacompayload.py

--- Wacom/deletewacompayload.py
+++ Wacom/deletewacompayload.py
@@ -19,12 +19,9 @@ class deletewacompayload(Processor):
 
         self.output ("whoami...")
         cmd = "sudo whoami"
-        exitcode, out, err = get_exitcode_stdout_stderr(cmd)
 
         self.output ("Removing protective ACLs from Wacom payload folder...")
         cmd = "sudo chmod -N /Users/administrador/Library/AutoPkg/Cache/com.github.aitorhergon.autopkg.pkg.wacom/downloads/payload"
-        exitcode, out, err = get_exitcode_stdout_stderr(cmd)
 
         self.output ("Deleting Wacom payload folder...")
         cmd = "sudo rm -rf /Users/administrador/Library/AutoPkg/Cache/com.github.aitorhergon.autopkg.pkg.wacom/downloads/payload"
-        exitcode, out, err = get_exitcode_stdout_stderr(cmd)
