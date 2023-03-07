e364b40b7be00754952b74e245c7a22c23b0bdb2
aitorhergon <hdezaitor@gmail.com>
2023-03-07 11:43:08 +0100

    Update deletewacompayload.py

--- Wacom/deletewacompayload.py
+++ Wacom/deletewacompayload.py
@@ -7,6 +7,7 @@ from autopkglib import Processor, ProcessorError
 
 __all__ = ["deletewacompayload"]
 
+
 class deletewacompayload(Processor):
 
     """This processor deletes your wacom payload folder."""
@@ -16,6 +17,12 @@ class deletewacompayload(Processor):
     description = __doc__
 
     def main(self):
-
-        self.output ("Deleting Wacom payload folder...")
-        os.system ("sudo rm -r /Users/administrador/Library/AutoPkg/Cache/com.github.aitorhergon.autopkg.pkg.wacom/downloads/payload")
+        try:
+            self.output ("Deleting Wacom payload folder...")
+            os.system ("sudo rm -r /Users/administrador/Library/AutoPkg/Cache/com.github.aitorhergon.autopkg.pkg.wacom/downloads/payload")
+        except Exception as err:
+            raise ProcessorError(err)
+        
+if __name__ == "__main__":
+    PROCESSOR = deletewacompayload()
+    PROCESSOR.execute_shell()
