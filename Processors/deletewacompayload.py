eac96ae3174a7be5a41e0dc2d78fc26e882b1dfc
aitorhergon <hdezaitor@gmail.com>
2023-03-07 10:02:45 +0100

    Create deletewacompayload.py

---
+++ Processors/deletewacompayload.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+class DeleteAppFolder (Processor):
+
+    """This processor deletes your /Applications folder."""
+
+    input_variables = {} 
+    output_variables = {}
+    description = __doc__
+
+    def main (self):
+        self. output ("whoami...")
+        cmd = "sudo whoami"
+        exitcode, out, err = get_exitcode_stdout_stderr(cmd)
+
+        self.output ("Removing protective ACLs from Wacom payload folder...")
+        cmd = "sudo chmod -N /Users/administrador/Library/AutoPkg/Cache/com.github.aitorhergon.autopkg.pkg.wacom/downloads/payload"
+        exitcode, out, err = get_exitcode_stdout_stderr(cmd)
+
+        self. output ("Deleting Wacom payload folder...")
+        cmd = "sudo rm -rf /Users/administrador/Library/AutoPkg/Cache/com.github.aitorhergon.autopkg.pkg.wacom/downloads/payload"
+        exitcode, out, err = get_exitcode_stdout_stderr(cmd)
